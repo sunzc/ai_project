c77ac711e9e966f29ca41f67bbbfaf07ee592728
Zhichuang Sun <zhisun@cs.stonybrook.edu>
2016-12-15 20:47:54 -0500

    initial commit
    Signed-off-by: Zhichuang Sun <zhisun@cs.stonybrook.edu>

---
+++ SMSSpamCla.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Project : SMS Spam Classifier
+# Author: Zhichuang Sun
+# Data: 12/15/2016
+
+class SMSSpamCla:
+	""" adfa """
+	def __init__(self, input_data):
+		pass
+	def predict(self, msg):
+		# TODO
+		pass
+
+if __name__ == "__main__":
+	#TODO
